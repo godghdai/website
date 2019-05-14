33daa7425c3456f8c56c53baeef4aeb495118a9b
godghdai <godghdai@gmail.com>
2019-05-14 15:24:35 +0800

    test

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 const path = require('path')
 
 const app = express()
- 
+
 
 app.use(express.static(path.join(__dirname, 'public')))
- 
+
 
 app.listen(3389, () => {
-  console.log(`App listening at port 3389`)
+    console.log(`App listening at port 3389`)
 })
