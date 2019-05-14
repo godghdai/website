43eb3d6c1bac791548264f73879c41142d543972
godghdai <godghdai@gmail.com>
2019-05-14 15:50:14 +0800

    ddd

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-﻿const express = require('express')
-
-const path = require('path')
-
-const app = express()
-
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-app.listen(3389, () => {
-    console.log(`App listening at port 3389`)
-})
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3389, () => { console.log(`App listening at port 3389`); });
